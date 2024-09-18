342f0301811b85684090ab46f76423fadd3c1769
dcyeager <dcyeager@hotmail.com>
2024-09-17 21:34:44 -0600

    csharp-prep/Prep1/Program.cs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Console.WriteLine("Hello Prep1 World!");
+
+        // Prompt user for thier first name
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+        // Prompt use for thier last name
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+        // Print the users full name
+        Console.WriteLine($"Your name is {firstname} {lastname}.");
     }
 }
