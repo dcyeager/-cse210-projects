abd30effad92d2a0b745bae88a7c47956ad099c8
dcyeager <dcyeager@hotmail.com>
2024-09-17 22:40:59 -0600

    Initial Code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Console.WriteLine("Hello Prep2 World!");
+        // Prompt user for thier grade percentage
+        Console.Write("What is your grade percentage(%)? ");
+        string gradepercenttext = Console.ReadLine();
+        // Convert string to int
+        int gradepercent = int.Parse(gradepercenttext);
+        // get ones place for grade sign
+        int graderemainder = gradepercent % 10;
+        // init variables
+        string grade = "";
+        string gradesign = "";
+
+        // Determine Grade Letter
+        if (gradepercent >= 90)
+        {
+             grade = "A";
+        }     
+        else if (gradepercent >= 80)
+        {
+             grade = "B";
+        }     
+        else if (gradepercent >= 70)
+        {
+             grade = "C";
+        }
+        else if (gradepercent >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+        // Determine Grade sign.
+        if (graderemainder >= 7 && gradepercent < 97 && gradepercent > 60)
+        {
+            gradesign = "+";
+        }
+        else if (graderemainder <= 3 && gradepercent > 60)
+        {
+            gradesign = "-";
+        }    
+        else
+        {
+            gradesign = ""; 
+        }
+        // Display Grade results
+        Console.WriteLine($"Your grade is {grade}{gradesign}.");       
     }
 }
+
