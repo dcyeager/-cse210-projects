571ea6f680c33f195240abf08ea1793171c294d8
dcyeager <dcyeager@hotmail.com>
2025-01-16 17:49:32 -0700

    Added code to Prep 3

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 50);
+
+        int guess = 0;
+        while (guess != magicNumber)
+        {
+                Console.Write("What is your guess?");
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+        }
     }
 }
